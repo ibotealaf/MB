864edc892e58c71287a9497c5d9fa90c165c8aa0
ibotealaf <uknowbrave@gmail.com>
2023-12-07 09:23:27 +0100

    chore: switched from the embedded task object to referencing task

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import crypto from 'crypto';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 const hash = encryption();
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
@@ -31,14 +31,24 @@ app.post('/register', async function (request, response) {
             error: 'Email and password required',
         });
     }
+
+    const userExist = await db.collection('Users').findOne({ email });
+
+    if (userExist) {
+        return response.status(409).json({
+            status: false,
+            message: 'Something went wrong when attempting to sign up',
+            error: 'Invalid sign up',
+        });
+    }
+
     const newUser = {
         name,
         email,
         password,
         tasks: [],
     };
-    const user = await db.collection('tasks').insertOne(newUser);
-    console.log(user);
+    const user = await db.collection('Users').insertOne(newUser);
 
     const userData = Object.assign({}, newUser);
     Object.defineProperty(userData, 'token', {
@@ -67,7 +77,7 @@ app.post('/register', async function (request, response) {
 app.post('/login', async function (request, response) {
     const { email, password } = request.body;
 
-    const user = await db.collection('tasks').findOne({ email });
+    const user = await db.collection('Users').findOne({ email });
 
     if (!(user && user.password == password)) {
         return response.status(422).json({
@@ -110,30 +120,55 @@ app.get('/tasks', async function (request, response) {
  * @route {host-url}/task/{id}
  * @api public
  */
-app.get('/tasks/:taskId', function (request, response) {
+app.get('/tasks/:taskId', async function (request, response) {
     const taskId = request.params.taskId;
     const user = request.user;
 
-    const task = user.tasks.find(function getTask(task) {
-        return task.id == taskId;
-    });
-    if (!task) {
-        return response.status(404).json({
+    if (taskId.length < 24) {
+        return response.status(422).json({
             status: false,
-            message: 'Task not found',
-            error: `Task with ID ${formattedTaskId} does not exist in your tasks`,
+            message: 'Something went wrong when attempting to fetch task.',
+            error: 'Malformed task id',
         });
     }
 
-    response.status(200).json({
-        status: true,
-        message: 'Retrieval successful',
-        data: {
-            ...task,
-        },
-    });
-});
+    try {
+        const task = await db
+            .collection('Tasks')
+            .findOne({ _id: new ObjectId(taskId) });
 
+        if (!task) {
+            return response.status(404).json({
+                status: false,
+                message: 'Task not found',
+                error: `Task with ID ${taskId} does not exist in your tasks`,
+            });
+        }
+
+        if (task && task.userId.toString() == user._id) {
+            return response.status(403).json({
+                status: false,
+                message: "You can't view this resource",
+                error: 'Permission denied',
+            });
+        }
+
+        response.status(200).json({
+            status: true,
+            message: 'Retrieval successful',
+            data: {
+                ...task,
+            },
+        });
+    } catch (err) {
+        console.error(err);
+        return response.status(500).json({
+            status: false,
+            message: 'Something went wrong!',
+            error: 'Internal server error',
+        });
+    }
+});
 /**
  * @description creates a new task
  * @method POST
@@ -163,14 +198,16 @@ app.post('/tasks', async function (request, response) {
         title,
         description,
         dueDate: dueDate ?? defaultDueDate,
-        id: generateId(),
         status: false,
+        userId: user._id,
     };
-    user.tasks.push(newTask);
 
     try {
+        const task = await db.collection('Tasks').insertOne(newTask);
+        user.tasks.push(task.insertedId.toString());
+
         const updatedUser = await db
-            .collection('tasks')
+            .collection('Users')
             .updateOne({ email: user.email }, { $set: { tasks: user.tasks } });
     } catch (err) {
         console.error(err);
@@ -192,40 +229,60 @@ app.post('/tasks', async function (request, response) {
 app.put('/tasks/:taskId', async function (request, response) {
     const taskId = request.params.taskId;
     const { title, description, dueDate, status } = request.body;
-
     const user = request.user;
-    const task = user.tasks.find(function getTask(task) {
-        return task.id == taskId;
-    });
-    if (!task) {
-        return response.status(404).json({
+
+    if (taskId.length < 24) {
+        return response.status(422).json({
             status: false,
-            message: 'Task does not exist or has been deleted',
-            error: `Task with ID ${formattedTaskId} does not exist in your tasks`,
+            message:
+                'Something went wrong. Task no longer exist or must have been deleted.',
+            error: 'Malformed task id',
         });
     }
-
-    if (title) task.title = title;
-    if (description) task.description = description;
-    if (dueDate) task.dueDate = dueDate;
-    if (status) task.status = !task.status;
-
     try {
+        const task = await db
+            .collection('Tasks')
+            .findOne({ _id: new ObjectId(taskId) });
+
+        if (!task) {
+            return response.status(404).json({
+                status: false,
+                message: 'Task does not exist or has been deleted',
+                error: `Task with ID ${taskId} does not exist in your tasks`,
+            });
+        }
+        if (task && task.userId.toString() == user._id) {
+            return response.status(403).json({
+                status: false,
+                message: "You can't make modification to this resource",
+                error: 'Permission denied',
+            });
+        }
+
+        if (title) task.title = title;
+        if (description) task.description = description;
+        if (dueDate) task.dueDate = dueDate;
+        if (status) task.status = !task.status;
+
         await db
-            .collection('tasks')
-            .updateOne({ email: user.email }, { $set: { tasks: user.tasks } });
+            .collection('Tasks')
+            .updateOne({ _id: task._id }, { $set: { ...task } });
+
+        response.status(202).json({
+            status: true,
+            message: 'Updated successfully',
+            data: {
+                ...task,
+            },
+        });
     } catch (err) {
         console.error(err);
-        return response.status(500).end();
+        return response.status(500).json({
+            status: false,
+            message: 'Something went wrong!',
+            error: 'Internal server error',
+        });
     }
-
-    response.status(202).json({
-        status: true,
-        message: 'Updated successfully',
-        data: {
-            ...task,
-        },
-    });
 });
 
 /**
@@ -236,36 +293,57 @@ app.put('/tasks/:taskId', async function (request, response) {
  */
 app.delete('/tasks/:taskId', async function (request, response) {
     const taskId = request.params.taskId;
-
     let user = request.user;
-    let task = user.tasks.find(function getTask(task) {
-        return task.id == taskId;
-    });
-    if (!task) {
-        return response.status(404).json({
+
+    if (taskId.length < 24) {
+        return response.status(422).json({
             status: false,
-            message: 'Task not found',
-            error: `Task with ID ${formattedTaskId} does not exist in your tasks`,
+            message:
+                'Something went wrong. Task no longer exist or must have been deleted.',
+            error: 'Malformed task id',
         });
     }
-
-    user.tasks = user.tasks.filter(function getTask(t) {
-        return t.id != task.id;
-    });
-
     try {
-        await db
-            .collection('tasks')
-            .updateOne({ email: user.email }, { $set: { tasks: user.tasks } });
+        const task = await db
+            .collection('Tasks')
+            .findOneAndDelete({ _id: new ObjectId(taskId) });
+
+        if (!task) {
+            return response.status(404).json({
+                status: false,
+                message: 'Task does not exist or has been deleted',
+                error: `Task with ID ${taskId} does not exist in your tasks`,
+            });
+        }
+        if (task && task.userId.toString() == user._id) {
+            return response.status(403).json({
+                status: false,
+                message: "You can't make modification to this resource",
+                error: 'Permission denied',
+            });
+        }
+
+        user.tasks = user.tasks.filter(function getTask(id) {
+            return id != task._id.toString();
+        });
+
+        const updatedUser = await db
+            .collection('Users')
+            .findOneAndUpdate(
+                { email: user.email },
+                { $set: { tasks: user.tasks } }
+            );
+
+        console.log(updatedUser);
+
+        response.status(204).json({
+            status: true,
+            message: 'Deleted successfully',
+        });
     } catch (err) {
         console.error(err);
         return response.status(500).end();
     }
-
-    response.status(204).json({
-        status: true,
-        message: 'Deleted successfully',
-    });
 });
 
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
@@ -320,7 +398,7 @@ async function detokenize(request, response, next) {
 
     try {
         const payload = hash.decrypt(token);
-        const user = await db.collection('tasks').findOne({ email: payload });
+        const user = await db.collection('Users').findOne({ email: payload });
         if (!user) {
             return response.status(401).json({
                 status: false,
